fee0b8ccb0539505d8d6a38e73ad9f1b6a5cefec
Anuj Deshmukh <82807676+anuj836@users.noreply.github.com>
2025-04-01 09:33:45 +0530

    Add lib/constants.dart 1743480225236

---
+++ lib/constants.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const Color kPrimaryColor = Color(0xFFE65100);
+const Color kSecondaryColor = Color(0xFFF57C00);
+const Color kBackgroundColor = Color(0xFFF5F5F5);
+const Color kTextColor = Color(0xFF212121);
+const Color kTextLightColor = Color(0xFF757575);
+
+// Pexels Image URLs
+const String kHeroImageUrl = 'https://images.pexels.com/photos/3761509/pexels-photo-3761509.jpeg';
+const String kBurgerCategoryImage = 'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg';
+const String kPizzaCategoryImage = 'https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg';
+const String kSushiCategoryImage = 'https://images.pexels.com/photos/2098085/pexels-photo-2098085.jpeg';
+const String kDessertCategoryImage = 'https://images.pexels.com/photos/2067396/pexels-photo-2067396.jpeg';
+
+// Product Images
+const List<String> kBurgerImages = [
+  'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg',
+  'https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg',
+];
+const List<String> kPizzaImages = [
+  'https://images.pexels.com/photos/825661/pexels-photo-825661.jpeg',
+  'https://images.pexels.com/photos/315755/pexels-photo-315755.jpeg',
+];
+
+// Text Styles
+const TextStyle kHeadingTextStyle = TextStyle(
+  fontSize: 28,
+  fontWeight: FontWeight.bold,
+  color: kTextColor,
+);
+
+const TextStyle kSubheadingTextStyle = TextStyle(
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+  color: kTextColor,
+);
+
+// Padding
+const double kDefaultPadding = 16.0;
